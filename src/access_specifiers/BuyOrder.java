ddd6aad9c6e6323ac2523e90cf1a4d0038cfcb83
kanchanchormage6079 <kanchanchormage9@gmail.com>
2026-06-19 17:31:39 +0530

    Update BuyOrder.java

--- src/access_specifiers/BuyOrder.java
+++ src/access_specifiers/BuyOrder.java
@@ -2,6 +2,9 @@ package access_specifiers;
 
 public class BuyOrder {
     static void main(String[] args) {
+        int a=5;
+        int b= a+4;
+        System.ouit.println(b);
         System.out.println("This is BuyOrder class in MyProject...");
     }
 }
